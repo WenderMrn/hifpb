aafb4164f485478bce56d7a9b28a6e1afff59722
Andre Alencar <andrealencar@outlook.com.br>
2017-07-30 14:09:27 -0300

    Ajuste na exibição dos horarios das aulas, e na exibição do formulario de edição

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,39 +4,14 @@ class Lesson < ApplicationRecord
   belongs_to :schoolroom, optional: true
   belongs_to :laboratory, optional: true
   belongs_to :campus_schedule
+  validates :day, uniqueness: { scope: [:campus_schedule, :classroom], message: "A turma já possui aula neste horário" }
+  validates :day, uniqueness: { scope: [:campus_schedule, :schoolroom], message: "Esta sala já está reservada neste horário" }
+  validates :day, uniqueness: { scope: [:campus_schedule, :laboratory], message: "Este laboratório já está reservado neste horário" }
   validate :there_must_be_one_of_the_two
-  validate :can_not_lesson_schedule_twice
-  validate :can_not_teach_a_lesson_twice
-
-
+  
   def there_must_be_one_of_the_two
     if (schoolroom == nil) && (laboratory == nil)
       errors.add(:Aula, "deve haver um laboratório ou uma sala")
     end
-  end
-
-  def can_not_lesson_schedule_twice
-    @classroom = Classroom.find(classroom_id)
-    @lessons = Lesson.where(classroom: @classroom)
-
-    @lessons.each{ |l|
-      if (l.day.include? day) && (l.campus_schedule_id == campus_schedule_id)
-        errors.add(:Falha,", já existe um horário reservado para este dia")
-      end
-    }
-  end
-  def can_not_teach_a_lesson_twice
-    @classroom = Classroom.find(classroom_id)
-    @teacher = @classroom.teacher_id
-    @lessons = Lesson.all
-
-    @lessons.each { |l|
-      if (l.classroom.teacher_id == @teacher)
-        if (l.day.include? day) && (l.campus_schedule_id == campus_schedule_id)
-          errors.add(:Falha,", já existe um horário reservado do professor para este dia")
-        end
-
-      end
-    }
   end
 end
