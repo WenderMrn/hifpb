3a56a230084b224eea7523b01aeca51919e4819c
Marianna Veríssimo <joffily@outlook.com>
2017-08-20 11:11:48 -0300

    Melhorias visuais na listagem de cursos

--- app/controllers/schedule/course_controller.rb
+++ app/controllers/schedule/course_controller.rb
@@ -1,7 +1,7 @@
 class Schedule::CourseController < SiteController
 
   def index
-    @courses = Course.where(campus: get_campi)
+    @courses = Course.where(campus: get_campi).order(name: :asc)
     @selected_campus = Campus.find(get_campi)
   end
 
@@ -10,7 +10,7 @@ class Schedule::CourseController < SiteController
       @course = Course.find_by(id: params[:course])
       @semester = params[:semester]
       @lessons = Lesson.by_semester(params[:semester]).by_course(params[:course])
-      @schedules = CampusSchedule.where(campus: Schoolroom.first.campus).order(:shift)
+      @schedules = CampusSchedule.where(campus: get_campi).order(:shift)
       @semesters = CourseSubject.semester_by_course(params[:course])
     end
   end
